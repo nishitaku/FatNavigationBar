2df4b91768627c90f2d89206934c921b8c6860c1
nishitaku <29690066+nishitaku@users.noreply.github.com>
2020-08-25 21:45:55 +0900

    NavigationControllerを使った画面遷移

--- FatNavigationBar/NextViewController.swift
+++ FatNavigationBar/NextViewController.swift
@@ -1,33 +1,21 @@
-//
-//  NextViewController.swift
-//  FatNavigationBar
-//
-//  Created by SRA01 on 2020/08/25.
-//  Copyright © 2020 nishitaku. All rights reserved.
-//
-
 import UIKit
 
 class NextViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.navigationItem.title = "Next"
+        let leftBarButton = UIBarButtonItem(title: "< Previous", style: .plain, target: self, action: #selector(tappedLeftBarButton))
+        self.navigationItem.setLeftBarButton(leftBarButton, animated: true)
     }
     
     @IBAction func back(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc private func tappedLeftBarButton() {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
-
+    
 }
--- FatNavigationBar/ViewController.swift
+++ FatNavigationBar/ViewController.swift
@@ -1,24 +1,17 @@
-//
-//  ViewController.swift
-//  FatNavigationBar
-//
-//  Created by SRA01 on 2020/08/25.
-//  Copyright © 2020 nishitaku. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     @IBAction func nextPage(_ sender: UIButton) {
         let nextViewController = self.storyboard?.instantiateViewController(withIdentifier: "NextViewController") as! NextViewController
-        nextViewController.modalPresentationStyle = .fullScreen
-        self.present(nextViewController, animated: true, completion: nil)
+        
+        let navigationController = UINavigationController(rootViewController: nextViewController)
+        navigationController.modalPresentationStyle = .fullScreen
+        self.present(navigationController, animated: true, completion: nil)
     }
     
 }
